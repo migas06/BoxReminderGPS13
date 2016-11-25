b79ac854a6a2ec841c6a024ecfed93666129a651
migas06 <miguelmartins018@gmail.com>
2016-11-25 15:18:00 +0000

    transições

--- app/src/main/java/com/isec/boxreminder/InserirRegisto.java
+++ app/src/main/java/com/isec/boxreminder/InserirRegisto.java
@@ -1,15 +1,32 @@
 package com.isec.boxreminder;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InserirRegisto extends ActionBarActivity
 {
+    Button next;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir_registo);
+
+        context = this;
+
+        next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, InserirRegisto.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/isec/boxreminder/MainActivity.java
+++ app/src/main/java/com/isec/boxreminder/MainActivity.java
@@ -1,10 +1,17 @@
 package com.isec.boxreminder;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity
 {
+
+    Button next;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,6 +19,15 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = this;
 
+        next = (Button) findViewById(R.id.menuButtonRegistarMedicamento);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, InserirRegisto.class);
+                startActivity(intent);
+            }
+        });
     }
 }

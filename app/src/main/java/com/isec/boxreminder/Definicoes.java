ff6116e4dd06e20aadc88a1f1edd1721fc38527e
peixaxo <joaoa13portugal@gmail.com>
2016-11-25 14:54:21 +0000

    Criação do Layout do menu inicial

--- app/src/main/java/com/isec/boxreminder/Definicoes.java
+++ app/src/main/java/com/isec/boxreminder/Definicoes.java
@@ -3,10 +3,12 @@ package com.isec.boxreminder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
-public class Definicoes extends ActionBarActivity {
+public class Definicoes extends ActionBarActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definicoes);
     }
--- app/src/main/java/com/isec/boxreminder/InserirQuantidade.java
+++ app/src/main/java/com/isec/boxreminder/InserirQuantidade.java
@@ -3,10 +3,12 @@ package com.isec.boxreminder;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class InserirQuantidade extends Activity {
+public class InserirQuantidade extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir_quantidade);
     }

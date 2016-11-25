92b5e6b6df99105903ddd02dbfd1ab4593d363f5
miguelaf24 <miguelaf24@gmail.com>
2016-11-25 15:19:16 +0000

    Ligaçao Definições com a MainActivity

--- app/src/main/java/com/isec/boxreminder/Definicoes.java
+++ app/src/main/java/com/isec/boxreminder/Definicoes.java
@@ -1,15 +1,29 @@
 package com.isec.boxreminder;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Definicoes extends ActionBarActivity
 {
 
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definicoes);
+
+        context = this;
+        ((Button)findViewById(R.id.definicoesBotaoVoltar)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(context, MainActivity.class);
+                startActivity(a);
+            }
+        });
     }
 }

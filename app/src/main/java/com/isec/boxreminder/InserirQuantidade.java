6667c20dbd54ffa63507e148966ea09ccb86af73
migas06 <miguelmartins018@gmail.com>
2016-11-25 03:20:29 +0000

    add Quantidade
    xml sem configuração

---
+++ app/src/main/java/com/isec/boxreminder/InserirQuantidade.java
@@ -0,0 +1,13 @@
+package com.isec.boxreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InserirQuantidade extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inserir_quantidade);
+    }
+}

d9af55d6ca72d0a73e6a6fe0956bc114c8c1c3b8
migas06 <miguelmartins018@gmail.com>
2016-11-29 18:01:30 +0000

    new data activity

---
+++ app/src/main/java/com/isec/boxreminder/Data.java
@@ -0,0 +1,13 @@
+package com.isec.boxreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Data extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}

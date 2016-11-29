bf6d5c5f14387b7cbeaf88080bc8fefc24460edd
migas06 <miguelmartins018@gmail.com>
2016-11-29 22:00:47 +0000

    Data activity update

--- app/src/main/java/com/isec/boxreminder/Data.java
+++ app/src/main/java/com/isec/boxreminder/Data.java
@@ -1,13 +1,85 @@
 package com.isec.boxreminder;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Calendar;
+
+import static android.R.attr.onClick;
 
 public class Data extends Activity {
+
+    Calendar c = Calendar.getInstance();
+    int startYear = c.get(Calendar.YEAR);
+    int startMonth = c.get(Calendar.MONTH);
+    int startDay = c.get(Calendar.DAY_OF_MONTH);
+
+    ImageView dataInicio;
+    ImageView dataFim;
+
+    EditText editTextDataInicio;
+    EditText editTextDataFim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+
+
+        editTextDataInicio = (EditText) findViewById(R.id.editTextDataInicio);
+        editTextDataFim    = (EditText) findViewById(R.id.editTextDataFim);
+
+        editTextDataInicio.setText(startDay + " - " + startMonth + " - " + startYear);
+
+        dataInicio = (ImageView) findViewById(R.id.imagemDataInicio);
+        dataFim    = (ImageView) findViewById(R.id.imagemDataFim);
+
+        dataInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment dialogFragment = new StartDatePicker();
+                dialogFragment.show(getFragmentManager(), "start_date_picker");
+            }
+        });
+
+        dataFim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment dialogFragment = new StartDatePicker();
+                dialogFragment.show(getFragmentManager(), "start_date_picker");
+            }
+        });
+
+    }
+
+    class StartDatePicker extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // TODO Auto-generated method stub
+            // Use the current date as the default date in the picker
+            DatePickerDialog dialog = new DatePickerDialog(Data.this, this, startYear, startMonth, startDay);
+            return dialog;
+
+        }
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            // Do something with the date chosen by the user
+            startYear = year;
+            startMonth = monthOfYear;
+            startDay = dayOfMonth;
+
+        }
     }
 }
+
+

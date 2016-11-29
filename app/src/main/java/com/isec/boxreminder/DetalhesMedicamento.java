bbb413ddef3d4908d01a7733338b0300602d1d86
miguelaf24 <miguelaf24@gmail.com>
2016-11-29 23:32:19 +0000

    Criação das vistas Ver Registo e Detalhes Medicamento

---
+++ app/src/main/java/com/isec/boxreminder/DetalhesMedicamento.java
@@ -0,0 +1,14 @@
+package com.isec.boxreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetalhesMedicamento extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_medicamento);
+
+    }
+}
---
+++ app/src/main/java/com/isec/boxreminder/VerRegistos.java
@@ -0,0 +1,13 @@
+package com.isec.boxreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VerRegistos extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_registos);
+    }
+}

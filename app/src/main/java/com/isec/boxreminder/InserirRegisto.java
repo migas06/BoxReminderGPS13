3743b7668249e42eb0c43fd330f604e3dee5e066
peixaxo <joaoa13portugal@gmail.com>
2016-12-02 23:03:57 +0000

    Teste unitário para class Definicoes

--- app/src/main/java/com/isec/boxreminder/InserirRegisto.java
+++ app/src/main/java/com/isec/boxreminder/InserirRegisto.java
@@ -3,17 +3,8 @@ package com.isec.boxreminder;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.media.SoundPool;
-import android.net.Uri;
 import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,9 +12,6 @@ import android.widget.Toast;
 
 import com.isec.boxreminder.Classes.Gravacao;
 import com.isec.boxreminder.Classes.Medicamento;
-
-import java.io.IOException;
-import java.io.Serializable;
 
 public class InserirRegisto extends Activity
 {
---
+++ app/src/test/java/com/isec/boxreminder/DefinicoesTest.java
@@ -0,0 +1,51 @@
+package com.isec.boxreminder;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by João André on 02/12/2016.
+ */
+
+public class DefinicoesTest
+{
+    private static Activity definicoes;
+
+    @BeforeClass
+    public static void setUp()
+    {
+        definicoes = new Definicoes();
+    }
+
+    @Before
+    public void definicoesTestBefore()
+    {
+        EditText mEditText = (EditText) definicoes.findViewById(R.id.editTextContacto);
+
+        mEditText.setText("919666333"); //número de telemóvel aleatório entre 200000000 e 969999999
+
+        long contacto = Long.parseLong(mEditText.getText().toString());
+
+        ((Button)definicoes.findViewById(R.id.definicoesBotaoVoltar)).performClick();
+    }
+
+    @Test
+    public void definicoesTest() throws AssertionError
+    {
+        assertTrue(definicoes.isFinishing());
+    }
+
+    @AfterClass
+    public static void finish()
+    {
+        definicoes = null;
+    }
+}
